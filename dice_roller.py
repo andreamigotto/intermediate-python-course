1c8170021c688b8c94bde4dd0cb11daf7bd7baac
Andrea Migotto <a.migotto@reply.it>
2020-09-24 17:20:30 +0200

    b

--- dice_roller.py
+++ dice_roller.py
@@ -1,8 +1,10 @@
 import random
 
 def main():
-  roll = random.randint(1,6)
-  print(f'You rolled a {roll}')
+  n_rolls = 2
+  for i in range(0,n_rolls):
+    roll = random.randint(1,6)
+    print(f'You rolled a {roll}')
 
 
 if __name__ == "__main__":
